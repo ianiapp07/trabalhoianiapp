214f4e80149c573d709861567a5188d14c07854e
ianiapp07 <130661323+ianiapp07@users.noreply.github.com>
2024-09-25 19:42:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Mecânica',
+    'O que significa as siglas presentes no oleo do motor?',
+    'Representa sua viscosidade em determinada temperatura.'
+)
+
+criaCartao(
+    'Mecânica',
+    'Andar na reserva queima abomba de combustível?',
+    'Não, isso é apenas um mito.'
+)
+
+criaCartao(
+    'Mecânica',
+    'Qual componente é responsavél por deixar o pedal de freio macio?',
+    'Hidrovácuo.'
+)
+
+criaCartao(
+    'Mecânica',
+    'Qual é o ciclo de um motor 4 cilindros?',
+    'Ciclo OTTO.'
+)
+
+criaCartao(
+    'Mecânica',
+    'Qual é a diferença entre motores diesel e gasolina?',
+    'Velas para a combustão do combustivel.'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que significa asigla CVT presente nos câmbios?',
+    'Transmissão continuamente variável.'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que significa a sigla ABS?',
+    'Anti-lock Braking System.'
+)
+
+criaCartao(
+    'Mecânica',
+    'Qual é o básico da manutenção automotiva?',
+    'Oléo, Cabos, Vela, Câmbio, Suspensão, Freios e Bicos Injetores.'
+)
+
+criaCartao(
+    'Mecânica',
+    'A suspensão precisa estar boa para realizar o alinhamento?',
+    'Sempre.'
+)
+
+criaCartao(
+    'Mecânica',
+    'Pneus interferem no alinhamento?',
+    'Sim, devem estar em boas condições, serem de mesma medida e marca.'
+)
